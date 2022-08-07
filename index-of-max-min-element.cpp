a3fac9ae4e34fe86bc14305a5072191fa09f2eec
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2022-08-07 06:32:00 +0530

    Adding More Algorithms && Data Structures 🚀

--- index-of-max-min-element.cpp
+++ index-of-max-min-element.cpp
@@ -5,9 +5,14 @@ int max_element_index(vector<int64_t> a){
     return int(max_element(a.begin(), a.end()) - a.begin());
 }
 
+int min_element_index(vector<int64_t> a){
+    return int(min_element(a.begin(), a.end()) - a.begin());
+}
+
+
 int main (){
-    vector<int64_t> a = {0, 1, 2, 69, 4, 5, 6, 7, 6, 5, 4, 3, 3, 2};
-    int maxdex = max_element_index(a);
+    vector<int64_t> a = {70, 1, 2, 69, 4, 5, 6, 7, 1, 6, 5, 4, 3, 3, 8};
+    int maxdex = min_element_index(a);
     cout << a[maxdex];
     return 0;    
 }
