d8a1157f2015a4ac191704c841325ee2cfa7c1d0
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2022-08-28 08:42:47 +0530

    Adding More Algorithms && Data Structures 🚀

--- output-matrix.cpp
+++ output-matrix.cpp
@@ -1,11 +1,3 @@
-/*    यो न हृष्यति न द्वेष्टि न शोचति न काङ्क्षति। शुभाशुभपरित्यागी भक्तिमान्यः स मे प्रियः॥
-``````````````````````````````````````````````````````````````````
- *    author:   abhijayrajvansh
- *    created:  28.06.2022 10:51:56
-**/
-#include <bits/stdc++.h>
-using namespace std;
-
 
 // output matrix N x M :-
 template <class T>
@@ -16,11 +8,4 @@ void printMatrix(vector<vector<T>> arr){
         }
         cout << '\n';
     }
-}
-
-int main (){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    return 0;    
 }
